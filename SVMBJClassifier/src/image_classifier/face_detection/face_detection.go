d037bd2adc076b573aefb893b8cedadcc0d32e49
Boon Jhee <wbjhee@gmail.com>
2021-07-22 22:47:01 +0800

    removed SVM, added CNN docs

--- SVMBJClassifier/src/image_classifier/face_detection/face_detection.go
+++ SVMBJClassifier/src/image_classifier/face_detection/face_detection.go
@@ -59,6 +59,8 @@ func main() {
 		for _, r := range rects {
 			gocv.Rectangle(&img, r, blue, 3)
 
+			imgFace := img.Region(r)
+			// caffe deep learning model on here
 			size := gocv.GetTextSize("Human", gocv.FontHersheyPlain, 1.2, 2)
 			pt := image.Pt(r.Min.X+(r.Min.X/2)-(size.X/2), r.Min.Y-2)
 			gocv.PutText(&img, "Human", pt, gocv.FontHersheyPlain, 1.2, blue, 2)
